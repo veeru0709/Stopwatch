41a4271379144ce1e6f3aaf75de8f74a259b20dd
Shubham Patel <veerupatel2002@gmail.com>
2024-05-28 21:24:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+import Stopwatch from './stopwatch';
+
+function App() {
+  
+  return(<><Stopwatch/>
+  <Stopwatch/></>
+);
+  
+}
+
+export default App
